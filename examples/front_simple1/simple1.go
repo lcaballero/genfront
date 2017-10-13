f1766ff7b1687fc36286a34e3db4d4350a15eaef
lcaballero <lucas.caballero@logrhythm.com>
2017-10-13 10:31:17 -0600

    Adding .gen.yyy to .gitignore to then accurately determine if generate is creating all the correct files.

--- examples/front_simple1/simple1.go
+++ examples/front_simple1/simple1.go
@@ -1,4 +1,13 @@
 package front_simple1
 
-//go:generate genfront front --input req_rest_methods.fm --output req_rest_methods.gen.go
+type Rest struct {}
+func (r *Rest) Method(method string) *Rest {
+	return r
+}
 
+type Req struct {}
+func (r *Req) Method(method string) *Req {
+	return r
+}
+
+//go:generate genfront front --input req_rest_methods.fm --output req_rest_methods.gen.go
---
+++ examples/plain_simple2/plain.go
@@ -0,0 +1,3 @@
+package plain_simple2
+
+//go:generate genfront plain --output plain.gen.go --template ../../embedded_files/di_logger.fm --data-file table:values.csv
